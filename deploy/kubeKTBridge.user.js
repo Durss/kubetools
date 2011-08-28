f11bfd6bdef895855eb51603c11bdbd9adedf5f4
durss <durss@022ad08b-5bc9-4080-825a-0c3df1df7a78>
2011-08-28 13:59:27 +0000

    Adding visitor mode and debugging gallery.

--- deploy/kubeKTBridge.user.js
+++ deploy/kubeKTBridge.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Kube bridge
-// @namespace     http://www.muxxu.free.fr/kube/apps/kubetools
+// @namespace     http://fevermap.org/kubetools
 // @include       http://kube.muxxu.com/
 // @include       http://kube.muxxu.com/?*
 // @description   Includes an LC SWF bridge to the kube game. This bridge provides communication between the game and the KubeTools application.
@@ -12,5 +12,5 @@ if (document.getElementById('swf_minimap') != null) {
 	swfRef = unsafeWindow.document.getElementById('swf_minimap');
 	swfRef.appendChild(bridgeRef);
 	
-	bridgeRef.innerHTML = '<embed type="application/x-shockwave-flash" src="http://www.muxxu.free.fr/kube/apps/kubetools/lcBridge.swf?v=6" width="1" height="1" allowScriptAccess="always" quality="low" bgcolor="#ff0000" id="kubeBridge" /><div style="font-size:8px; position:relative; top:110px;">KB</div>';
+	bridgeRef.innerHTML = '<embed type="application/x-shockwave-flash" src="http://fevermap.org/kubetools/lcBridge.swf?v=7" width="1" height="1" allowScriptAccess="always" quality="low" bgcolor="#ff0000" id="kubeBridge" /><div style="font-size:8px; position:relative; top:110px;">KB</div>';
 }
