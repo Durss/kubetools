a9e465e73d5f512815651850921b4591d29c2bda
François Dursus <durss@free.fr>
2012-02-20 00:22:03 +0000

    Fixing greasemonkey scripts

--- deploy/kubeKTBridge.user.js
+++ deploy/kubeKTBridge.user.js
@@ -10,7 +10,10 @@ if (document.getElementById('swf_minimap') != null) {
 	var swfRef, bridgeRef;
 	bridgeRef= unsafeWindow.document.createElement('div');
 	swfRef = unsafeWindow.document.getElementById('swf_minimap');
-	swfRef.appendChild(bridgeRef);
 	
 	bridgeRef.innerHTML = '<embed type="application/x-shockwave-flash" src="http://fevermap.org/kubetools/lcBridge.swf?v=7" width="1" height="1" allowScriptAccess="always" quality="low" bgcolor="#ff0000" id="kubeBridge" /><div style="font-size:8px; position:relative; top:110px;">KB</div>';
+	bridgeRef.style.width = "20px";
+	bridgeRef.style.height = "20px";
+	
+	swfRef.appendChild(bridgeRef);
 }
